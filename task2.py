efd895110ea1081348044d4404f45ed88c660080
Luba <bart6891@ya.ru>
2022-12-15 15:04:34 +0300

    task2

---
+++ task2.py
@@ -0,0 +1,7 @@
+# 2. Напишите программу, которая на вход принимает 5 чисел и находит максимальное из них.
+num_max = 0
+for i in range (5):
+    num = int(input())
+    if num_max < num:
+        num_max = num
+print(num_max)
